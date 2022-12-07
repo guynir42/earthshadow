2ef5ef48a3ebed2817812c9ddfdceb5b6d69c516
Guy Nir <guyynir@gmail.com>
2022-12-07 15:07:24 -0800

    beef up tests

--- earthshadow.py
+++ earthshadow.py
@@ -9,18 +9,91 @@ import astropy.units as u
 
 import matplotlib.pyplot as plt
 
-# just for fun let's use ZTF's coordinates
-DEFAULT_OBS_LONGITUDE = 116.8356 * u.deg
-DEFAULT_OBS_LATITUDE = 33.3634 * u.deg
-DEFAULT_OBS_ALTITUDE = 1.872 * u.km
+EARTH_RADIUS = 6371 * u.km
+LOW_EARTH_ORBIT = 300 * u.km + EARTH_RADIUS
+MEDIUM_EARTH_ORBIT = 1000 * u.km + EARTH_RADIUS
+GPS_SATELITE_ORBIT = 26600 * u.km
+GEOSYNCHRONOUS_ORBIT = 42164 * u.km
+HIGH_EARTH_ORBIT = GEOSYNCHRONOUS_ORBIT * 2
 
-DEFAULT_ORBITAL_RADIUS = 42164 * u.km
+DEFAULT_ORBITAL_RADIUS = GEOSYNCHRONOUS_ORBIT
 
 
-def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=False):
+def get_earth_shadow(orbit=None, geocentric_orbit=True, geocentric_angle=True):
     """
-    Calculate the distance from the center of the shadow
+    Get the angle of the radius of Earth's shadow,
+    where it intercepts the sky at an orbital radius,
+    as seen by an observer at the center of the Earth.
+
+    This is the geometric shadow, and does not include
+    partial shadowing by the atmosphere.
+    The atmosphere allows bending of some light to reach
+    an angle of about 1-2 degrees into the geometric shadow.
+
+    When inputting the orbit as a float (assume km) or a Quantity
+    it is assumed this value includes the radius of the Earth.
+    To give the orbit height above the Earth's surface,
+    specify geocentric=False.
+
+    Parameters
+    ----------
+    orbit: float or astropy.units.Quantity
+        The orbital radius of the satellite.
+        This is measured from the center of the Earth
+        (e.g., LEO would be 200 + 6371 = 6571 km).
+        If given as float assume kilometers.
+        Defaults to 42164 km (geosynchronous orbit).
+    geocentric_orbit: bool
+        If True, assume the orbit is given as the
+        distance from the center of the Earth (default).
+        If False, assume the orbit is given as the
+        distance above the Earth's surface.
+
+    Returns
+    -------
+    angle: astropy.units.Quantity
+        The angle of the Earth shadow.
+    """
+    orbit = interpret_orbit(orbit, geocentric=geocentric_orbit)
+
+    if orbit < EARTH_RADIUS:
+        raise ValueError(
+            f"Orbit radius {orbit} is below Earth radius {EARTH_RADIUS}. "
+            "If you intend to give the orbit height above the Earth surface, "
+            "set geocentric_orbit=False."
+        )
+
+    angle = np.arcsin(EARTH_RADIUS / orbit).to(u.deg)
+
+    if not geocentric_angle:
+        angle = geocentric_to_topocentric_angle(angle, orbit=orbit)
+
+    return angle
+
+
+def dist_from_shadow_center(
+    ra,
+    dec,
+    time=None,
+    orbit=None,
+    obs=None,
+    geocentric_orbit=True,
+    geocentric_output=True,
+    verbose=False,
+):
+    """
+    Calculate the angular distance from the center of the shadow
     for the given coordinates at the given time.
+    The angle is measured by an observer at the center of the Earth
+    (default, but can also choose geocentric_output=False).
+    This should be compared to the size of the shadow from the same
+    vantage point, as given by the `get_earth_shadow` function.
+
+    For an orbit at infinite height,
+    the angular distance is the same as
+    the distance from the anti-solar point.
+    For a finite orbit radius (e.g., GEO)
+    this also incorporates the effects of parallax.
 
     Parameters
     ----------
@@ -35,9 +108,25 @@ def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=Fa
         The orbital radius at which we assume the targets are moving.
         The lower the orbit, the bigger the Earth's shadow will be.
         Default is 42164 km, which is the altitude of geosynchronous satellites.
-    obs: astropy.coordinates.SkyCoord or 3-tuple of (lon, lat, alt)
+    obs: astropy.coordinates.EarthLocation, string, or 3-tuple of (lon, lat, alt)
         The coordinates of the observatory.
         Default is the coordinates of the ZTF observatory.
+        If given as string will use the
+        `astropy.coordinates.EarthLocation.of_site` method.
+        If given as a list or tuple, will assume it is
+        (longitude, latitude, altitude) in degrees and meters.
+
+    geocentric_orbit: bool
+        If True, assume the orbit is given as the
+        distance from the center of the Earth (default).
+        If False, assume the orbit is given as the
+        distance above the Earth's surface.
+    geocentric_output: bool
+        Output the angle as measured by an observer at
+        the center of the Earth (default).
+        If False, will output the angle as measured by
+        an observer on the Earth's surface, assuming
+        the observer is under the center of the shadow.
     verbose: bool
         Whether to print various intermediate results.
 
@@ -45,6 +134,7 @@ def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=Fa
     -------
     dist: scalar float or array of floats
         The distance from the center of the shadow in degrees.
+        The angle is measured by an observer at the center of the Earth.
          (output is same size as ra/dec)
     """
 
@@ -61,46 +151,52 @@ def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=Fa
         dec *= u.deg
 
     # verify/convert the time
-    if time is None:
-        time = Time.now()
-    if isinstance(time, float):  # assume JD??
-        time = Time(time, format="jd")
-    if isinstance(time, datetime.datetime):
-        time = Time(time)
+    time = interpret_time(time)
 
     # verify/convert the orbit
-    if orbit is None:
-        orbit = DEFAULT_ORBITAL_RADIUS
-
-    if not isinstance(orbit, u.quantity.Quantity):
-        orbit *= u.km
+    orbit = interpret_orbit(orbit, geocentric=geocentric_orbit)
 
     # verify/convert the observatory coordinates
     if obs is None:
+        obs = coord.EarthLocation.of_site("palomar")
+    if isinstance(obs, str):
+        obs = coord.EarthLocation.of_site(obs)
+    if isinstance(obs, (list, tuple)):
+        if len(obs) != 3:
+            raise ValueError("obs must be a 3-tuple or list of (lon, lat, alt)")
+        new_obs = list(obs)
+        if not isinstance(new_obs[0], u.quantity.Quantity):
+            new_obs[0] = new_obs[0] * u.deg
+        if not isinstance(new_obs[1], u.quantity.Quantity):
+            new_obs[1] = new_obs[1] * u.deg
+        if not isinstance(new_obs[2], u.quantity.Quantity):
+            new_obs[2] = new_obs[2] * u.m
+
         obs = coord.EarthLocation.from_geodetic(
-            lon=DEFAULT_OBS_LONGITUDE,
-            lat=DEFAULT_OBS_LATITUDE,
-            height=DEFAULT_OBS_ALTITUDE,
-        )
-    if isinstance(obs, tuple):
-        obs = coord.EarthLocation.from_geodetic(
-            lon=obs[0],
-            lat=obs[1],
-            height=obs[2],
+            lon=new_obs[0],
+            lat=new_obs[1],
+            height=new_obs[2],
         )
     if not isinstance(obs, coord.EarthLocation):
         raise ValueError(
             "obs must be a 3-tuple of (lon, lat, alt) "
+            "or a string with a name of a known observatory "
             "or an astropy.coordinates.EarthLocation"
         )
 
-    # get the anti-sun position in geo-centric ecliptic coordinates
+    if verbose:
+        print(f"observatory: {obs}")
+
+    # get the anti-sun position in geo-centric coordinates
     anti_sun = get_anti_sun(time)
     if verbose:
         print("anti-sun:", anti_sun)
 
     # convert to astropy coordinates
-    target_coords = SkyCoord(ra, dec, frame="geocentrictrueecliptic", obstime=time)
+    obs_frame = coord.CIRS(obstime=time, location=obs)
+    target_coords = SkyCoord(ra, dec, frame=obs_frame)
+    if verbose:
+        print(f"target_coords = {target_coords}")
 
     # we can express these coordinates as the observer location x0, y0, z0 plus
     # a vector t that points from the observer to the target (xt, yt, zt)
@@ -123,8 +219,8 @@ def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=Fa
     # which gives the range from observer to target
     term_a = x0 * xt + y0 * yt + z0 * zt
     term_b = x0**2 + y0**2 + z0**2 - orbit**2
-    range_plus = term_a + np.sqrt(term_a**2 - term_b)  # km
-    range_minus = term_a - np.sqrt(term_a**2 - term_b)  # km
+    range_plus = -term_a + np.sqrt(term_a**2 - term_b)  # km
+    range_minus = -term_a - np.sqrt(term_a**2 - term_b)  # km
 
     prefer_plus_range = abs(range_plus) < abs(range_minus)
 
@@ -134,22 +230,39 @@ def dist_from_shadow_center(ra, dec, time=None, orbit=None, obs=None, verbose=Fa
             f"prefer_plus_range= {prefer_plus_range}"
         )
 
-    range = np.where(prefer_plus_range, range_plus, range_minus)
+    chosen_range = np.where(prefer_plus_range, range_plus, range_minus)
+
+    x_final = x0 + chosen_range * xt
+    y_final = y0 + chosen_range * yt
+    z_final = z0 + chosen_range * zt
+
+    if verbose:
+        print(
+            f"x= {x_final}, y= {y_final}, z= {z_final}, "
+            f"R= {np.sqrt(x_final**2 + y_final**2 + z_final**2)}"
+        )
 
     new_target_coords = SkyCoord(
-        x=x0 + range * xt,
-        y=y0 + range * yt,
-        z=z0 + range * zt,
-        frame="geocentrictrueecliptic",
-        obstime=time,
+        x=x_final,
+        y=y_final,
+        z=z_final,
+        frame=coord.GCRS(obstime=time),  # geocentric
         representation_type="cartesian",
-    ).transform_to("icrs")
+    )
+    new_target_coords.representation_type = "spherical"
 
     if verbose:
         print(f"new target: {new_target_coords}")
 
     # now we can compute the distance from the anti-sun
     dist = new_target_coords.separation(anti_sun)
+
+    if not geocentric_output:
+        # convert to the distance from the Earth's surface
+        dist = geocentric_to_topocentric_angle(dist, orbit)
+
+    if verbose:
+        print(f"dist = {dist}")
 
     return dist
 
@@ -168,16 +281,18 @@ def get_anti_sun(time=None):
     anti_sun: astropy.coordinates.SkyCoord
         The anti-sun position.
     """
+    time = interpret_time(time)
     sun = coord.get_sun(time)
     anti_sun = SkyCoord(
         ra=np.mod(sun.ra + 180 * u.deg, 360 * u.deg),
         dec=-sun.dec,
+        frame=coord.GCRS(obstime=time),  # geocentric
     )
 
     return anti_sun
 
 
-def get_observer_opposite_sun(time=None, altitutde=None):
+def get_observer_opposite_sun(time=None, altitude=None):
     """
     Get the geolocation of an observer that is
     right under the anti-sun point.
@@ -187,6 +302,10 @@ def get_observer_opposite_sun(time=None, altitutde=None):
     time: astropy.time.Time
         The time of the observation.
         Defaults to now.
+    altitude: astropy.units.Quantity
+        The altitude of the observer above sea level.
+        Defaults to 0. If given as float assume meters.
+
     Returns
     -------
     obs: astropy.coordinates.EarthLocation
@@ -196,25 +315,182 @@ def get_observer_opposite_sun(time=None, altitutde=None):
     if time is None:
         time = Time.now()
 
-    if altitutde is None:
-        altitutde = DEFAULT_OBS_ALTITUDE
+    if altitude is None:
+        altitude = 0
+
+    if not isinstance(altitude, u.quantity.Quantity):
+        altitude *= u.m
 
     anti_sun = get_anti_sun(time)
     obs = coord.EarthLocation.from_geodetic(
         lon=anti_sun.ra,
         lat=anti_sun.dec,
-        height=altitutde,
+        height=altitude,
     )
 
     return obs
 
 
+def topocentric_to_geocentric_angle(angle, orbit=None, geocentric_orbit=True):
+    """
+    Convert the angle above an observer
+    on the surface of the Earth to an angle
+    seen by an observer at the center of the Earth.
+    Assume the angle is seen straight over the observer.
+
+    This calculation uses the sine function,
+    which assumes the orbital radius is calculated
+    at the edges of a circle with a radius equal to "angle".
+    The center of this circle is right above the observer,
+    but is at a slightly lower altitude than the orbit given.
+    If the center of the circle was at the orbit
+    (and the edges higher than the orbit),
+    then the calculation should use the tangent function.
+    For small angles this makes no difference.
+
+    Parameters
+    ----------
+    angle: float or astropy.units.Quantity
+        The angle above the observer.
+        If given as float assume degrees.
+    orbit: float or astropy.units.Quantity
+        The orbital radius of the satellite.
+        If given as float assume kilometers.
+        Defaults to 42164 km (geosynchronous orbit).
+
+    Returns
+    -------
+    angle: astropy.units.Quantity
+        The angle above the observer.
+    """
+    if not isinstance(angle, u.quantity.Quantity):
+        angle *= u.deg
+
+    orbit = interpret_orbit(orbit, geocentric=geocentric_orbit)
+
+    return np.arcsin(np.sin(angle) * orbit / (orbit + EARTH_RADIUS)).to(u.deg)
+
+
+def geocentric_to_topocentric_angle(angle, orbit=None, geocentric_orbit=True):
+    """
+    Convert the angle above an observer
+    at the center of the Earth to an angle
+    seen by an observer on the surface of the Earth.
+    Assume the angle is seen straight over the observer.
+
+    This calculation uses the sine function,
+    which assumes the orbital radius is calculated
+    at the edges of a circle with a radius equal to "angle".
+    The center of this circle is right above the observer,
+    but is at a slightly lower altitude than the orbit given.
+    If the center of the circle was at the orbit
+    (and the edges higher than the orbit),
+    then the calculation should use the tangent function.
+    For small angles this makes no difference.
+
+    Parameters
+    ----------
+    angle: float or astropy.units.Quantity
+        The angle above the observer.
+        If given as float assume degrees.
+    orbit: float or astropy.units.Quantity
+        The orbital radius of the satellite.
+        If given as float assume kilometers.
+        Defaults to 42164 km (geosynchronous orbit).
+
+    Returns
+    -------
+    angle: astropy.units.Quantity
+        The angle above the observer.
+    """
+    if not isinstance(angle, u.quantity.Quantity):
+        angle *= u.deg
+
+    orbit = interpret_orbit(orbit, geocentric=geocentric_orbit)
+
+    return np.arcsin(np.sin(angle) * (orbit + EARTH_RADIUS) / orbit).to(u.deg)
+
+
+def interpret_time(time=None):
+    """
+    Interpret the user input for time
+    in various formats and return it
+    as an astropy.time.Time object.
+
+    If given as None (or not given)
+    will return the current time.
+    Can also can use the string "now".
+    """
+    if time is None:
+        time = Time.now()
+    if isinstance(time, str) and time.lower() == "now":
+        time = Time.now()
+    if isinstance(time, str):
+        time = Time(time)
+    if isinstance(time, float):  # assume JD??
+        time = Time(time, format="jd", scale="utc")
+    if isinstance(time, datetime.datetime):
+        time = Time(time)
+
+    return time
+
+
+def interpret_orbit(orbit=None, geocentric=True):
+    """
+    Convert user inputs for the orbital radius
+    into a quantity with units of kilometers.
+
+    Parameters
+    ----------
+    orbit: float or astropy.units.Quantity
+        The orbital radius of the satellite.
+        If given as float assume kilometers.
+    geocentric: bool
+        If True, assume the orbit is measured
+        from the center of the Earth (default).
+        To measure the orbit height above the
+        Earth's center use geocentric=False.
+
+    Returns
+    -------
+    orbit: astropy.units.Quantity
+        The orbital radius of the satellite.
+    """
+    if orbit is None:
+        orbit = DEFAULT_ORBITAL_RADIUS
+
+    if isinstance(orbit, str):
+        orbit = orbit.lower().replace(" ", "").replace("_", "").replace("-", "")
+        if orbit in ("geosynchronous", "geosync", "geo"):
+            orbit = DEFAULT_ORBITAL_RADIUS
+        elif orbit in ("lowearthorbit", "lowearth", "leo"):
+            orbit = LOW_EARTH_ORBIT
+        elif orbit in ("mediumearthorbit", "mediumearth", "meo"):
+            orbit = MEDIUM_EARTH_ORBIT
+        elif orbit in ("highearthorbit", "highearth", "heo"):
+            orbit = HIGH_EARTH_ORBIT
+        elif orbit in ("groundpositioningsatellite", "groundpositioning", "gps"):
+            orbit = GPS_SATELITE_ORBIT
+
+    if not isinstance(orbit, u.quantity.Quantity):
+        orbit *= u.km
+
+    if not geocentric:
+        orbit += EARTH_RADIUS
+
+    return orbit
+
+
 if __name__ == "__main__":
-    time = Time("2022-09-21T00:00:00")
+    time = Time("2023-09-23T06:50:00")
 
     anti = get_anti_sun(time)
     obs = get_observer_opposite_sun(time)
     ret = dist_from_shadow_center(
-        ra=anti.ra, dec=anti.dec, time=time, obs=obs, verbose=True
+        ra=anti.ra + 0.0 * u.deg,
+        dec=anti.dec + 5.0 * u.deg,
+        time=time,
+        obs=(0, 0, 0),
+        verbose=True,
+        orbit=None,
     )
-    print(ret)
--- tests.py
+++ tests.py
@@ -5,7 +5,96 @@ from astropy.time import Time
 from astropy.coordinates import SkyCoord
 import astropy.units as u
 
-from earthshadow import get_anti_sun, get_observer_opposite_sun, dist_from_shadow_center
+from earthshadow import *
+
+
+def test_user_inputs():
+    # test the time inputs
+    t0 = Time.now()
+    t1 = datetime.datetime.utcnow()
+
+    # assume if the input to get_anti_sun is equivalent,
+    # the output will be equivalent
+    a1 = get_anti_sun(t0)  # should work as is
+    a2 = get_anti_sun(t0.jd)  # assume float is JD
+    a3 = get_anti_sun()  # default should be now
+    a4 = get_anti_sun(t1)  # datetime should work
+
+    assert a1.separation(a2).to(u.arcsec) < 1 * u.arcsec
+    assert a1.separation(a3).to(u.arcsec) < 1 * u.arcsec
+    assert a1.separation(a4).to(u.arcsec) < 1 * u.arcsec
+
+    # test the orbit inputs
+    a1 = geocentric_to_topocentric_angle(5)  # default to GEO?
+    a2 = geocentric_to_topocentric_angle(5, orbit=42164)
+    a3 = geocentric_to_topocentric_angle(5, orbit=42164 * u.km)
+    a4 = geocentric_to_topocentric_angle(5, orbit="GEO")
+
+    assert isinstance(a1, u.Quantity)
+    assert a1.unit == "deg"
+    assert np.isclose(a1.value, a2.value)
+    assert np.isclose(a1.value, a3.value)
+    assert np.isclose(a1.value, a4.value)
+
+    # test the RA/Dec inputs
+    ra = 10
+    dec = 20
+    a1 = dist_from_shadow_center(ra, dec)
+    a2 = dist_from_shadow_center(ra * u.deg, dec * u.deg)
+    a3 = dist_from_shadow_center([ra], [dec])
+    a4 = dist_from_shadow_center(np.array([ra]), np.array([dec]))
+    a5 = dist_from_shadow_center(np.array([ra]) * u.deg, np.array([dec]) * u.deg)
+
+    assert abs(a1 - a2) < 0.01 * u.deg
+    assert abs(a1 - a3) < 0.01 * u.deg
+    assert abs(a1 - a4) < 0.01 * u.deg
+    assert abs(a1 - a5) < 0.01 * u.deg
+
+    # test the observatory inputs
+    b1 = dist_from_shadow_center(ra, dec, obs="Palomar")
+    b2 = dist_from_shadow_center(ra, dec, obs=(-116.863, 33.356, 1700))
+
+    assert abs(a1 - b1) < 0.01 * u.deg
+    assert abs(a1 - b2) < 0.01 * u.deg
+
+    # this should be a different place
+    c1 = dist_from_shadow_center(ra, dec, obs="Paranal Observatory")
+    assert abs(a1 - c1) > 1 * u.deg
+
+
+def test_earth_shadow_sizes():
+    # default value should be GEO
+    a = get_earth_shadow()
+    assert abs(a.value - 8.7) < 0.1
+
+    a1 = get_earth_shadow("GEO")
+    assert abs(a1 - a) < 0.1 * u.deg
+
+    # this is also close to GEO
+    a2 = get_earth_shadow(42000)
+    assert abs(a2 - a) < 0.1 * u.deg
+
+    # this is about twice Earth's radius
+    a3 = get_earth_shadow(13000)
+    assert a3 > 2 * a
+
+    # this is lower than Earth's radius
+    with pytest.raises(ValueError) as e:
+        get_earth_shadow(6000)
+        assert "is below Earth radius" in str(e)
+
+    # should work if geocentric_orbit=False
+    a4 = get_earth_shadow(6000, geocentric_orbit=False)
+
+    assert a4 > 3 * a
+
+    # what about low Earth orbit?
+    a5 = get_earth_shadow(300, geocentric_orbit=False)
+    assert a5 > 5 * a
+
+    # make sure the keywords work in geocentric too
+    a6 = get_earth_shadow("LEO", geocentric_orbit=True)
+    assert abs(a6 - a5) < 0.1 * u.deg
 
 
 def test_observer_under_shadow():
@@ -15,3 +104,30 @@ def test_observer_under_shadow():
     ret = dist_from_shadow_center(ra=anti.ra, dec=anti.dec, time=time, obs=obs)
 
     assert ret < 0.1 * u.deg
+
+    offset = 10 * u.deg
+    ret = dist_from_shadow_center(ra=anti.ra + offset, dec=anti.dec, time=time, obs=obs)
+
+    # return value is measured from the center of the Earth,
+    # while the offset angle is measured by a topocentric observer!
+
+    assert abs(ret.deg - 10) < 0.1
+
+
+def test_topocentric_to_geocentric():
+    angle = 5
+
+    assert abs(4.31 - topocentric_to_geocentric_angle(angle).value) < 0.1
+    assert abs(5.75 - geocentric_to_topocentric_angle(angle).value) < 0.1
+
+    # check we get the same results with explicitly passing the orbit
+    assert abs(4.31 - topocentric_to_geocentric_angle(angle, orbit=42000).value) < 0.1
+    assert abs(5.75 - geocentric_to_topocentric_angle(angle, orbit=42000).value) < 0.1
+
+    # check it works for low orbit (about 1 Earth radius)
+    assert abs(3.35 - topocentric_to_geocentric_angle(angle, orbit=13000).value) < 0.1
+    assert abs(7.46 - geocentric_to_topocentric_angle(angle, orbit=13000).value) < 0.1
+
+    # check the angle doesn't change for very high orbits
+    assert abs(5.0 - topocentric_to_geocentric_angle(angle, orbit=1e6).value) < 0.1
+    assert abs(5.0 - geocentric_to_topocentric_angle(angle, orbit=1e6).value) < 0.1
